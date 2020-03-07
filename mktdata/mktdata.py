02012e4d5a79214d09770cbe21503e4fda5b8b70
joeylam <joey.cm.lam@gmail.com>
2020-03-08 01:04:13 +0800

    add web services

--- mktdata/mktdata.py
+++ mktdata/mktdata.py
@@ -1,31 +1,32 @@
 import yfinance as yf
-import pprint
 from datetime import date
 
-def getTodayPrice(stockCode):
-    today = date.today()
-    latestDate = today.strftime("%Y-%m-%d")
+#class mktPrice:
 
+
+    #def __init__(self):
+        # body of the constructor
+
+
+def getTodayPrice(stockCode):
+    latestDate = date.today().strftime("%Y-%m-%d")
     price = getPrice(stockCode, latestDate, latestDate)
+
 
     return price
 
 def getPrice(stockCode, fromDate, toDate):
     closingPrice = 0
 
-    listStockCodes = "0001.HK " + stockCode
+    listStockCodes = "PFF " + stockCode
     data = yf.download(listStockCodes, start=fromDate, end=toDate, group_by="ticker")
 
     values = data[stockCode]['Close']
 
-    pprint.pprint(values)
+    #pprint.pprint(values)
 
     price_list = values.tolist()
 
     closingPrice = price_list[0]
 
-
-
     return closingPrice
-
-
