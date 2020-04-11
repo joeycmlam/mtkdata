a50f08c8fd7ad1123a85a76f2606445cfb22d1d4
joeylam <joey.cm.lam@gmail.com>
2020-04-12 01:48:58 +0800

    reflectoring

--- src/mktdata.py
+++ src/mktdata.py
@@ -2,12 +2,7 @@ import pprint
 
 import yfinance as yf
 from datetime import datetime, timedelta
-
-#class mktPrice:
-
-
-    #def __init__(self):
-        # body of the constructor
+import json
 
 
 def getTodayPrice(stockCode):
@@ -16,25 +11,17 @@ def getTodayPrice(stockCode):
     latestDate = datetime.now().strftime("%Y-%m-%d")
     fromDate = datetime.strftime(datetime.now() - timedelta(2), '%Y-%m-%d')
 
-    result = getStockInfo(stockCode, fromDate, latestDate)
+    result = getPriceInfo(stockCode, fromDate, latestDate)
 
-    price_list = result['Close']
-
-    num_record = len(price_list)
+    num_record = len(result)
     if num_record == 0:
-        pprint.pprint("price_list[] is empty")
+        pprint.pprint("result[] is empty")
     else:
-        closingPrice = price_list[num_record-1]
+        closingPrice = result[num_record-1]
 
     return closingPrice
 
 
-
-def getCurrentStockInfo(stockCode):
-    latestDate = datetime.now().strftime("%Y-%m-%d")
-    fromDate = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
-    values = getStockInfo(stockCode, fromDate, latestDate)
-    return values
 
 def getStockInfo(stockCode, fromDate, toDate):
     listStockCodes = "T " + stockCode
@@ -43,35 +30,30 @@ def getStockInfo(stockCode, fromDate, toDate):
     data = yf.download(listStockCodes, start=fromDate, end=toDate, group_by="ticker")
 
     pprint.pprint("getStockInfo: data {0}".format(data))
-    #values = data[stockCode]['Close']
 
     return data[stockCode]
 
-def getPrice(stockCode, fromDate, toDate):
-    closingPrice = 0
 
 
-    values = getStockInfo(stockCode, fromDate, toDate)
-    pprint.pprint(values)
+def getTodayStockInfo(stockCode):
+    latestDate = datetime.now().strftime("%Y-%m-%d")
+    fromDate = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
 
-    price_list = values
+    result = getStockInfo(stockCode, fromDate, latestDate)
 
-    if price_list == []:
-        pprint.pprint("price_list[] is empty")
+    num_record = len(result)
+    if num_record == 0:
+        pprint.pprint("result[] is empty")
     else:
-        closingPrice = price_list[len(price_list)-1]
+        rtnValue = result['Close'].tolist()
 
-    return closingPrice
+    return json.dumps(rtnValue)
+
 
 
 def getPriceInfo(stockCode, fromDate, toDate):
-    closingPrice = 0
-
 
     values = getStockInfo(stockCode, fromDate, toDate)
-    pprint.pprint(values)
-
-    #price_list = values.tolist()
 
     result = values['Close'].tolist()
     return result
